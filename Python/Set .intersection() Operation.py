26c63f8976edfd4dc3e93be75df5eecca6af30b2
brunhs <brunoschock@hotmail.com>
2020-12-15 19:14:26 -0400

    one answer more

---
+++ Python/Set .intersection() Operation.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+_ = int(input())
+list_1 = set(input().split())
+__ = int(input())
+list_2 = set(input().split())
+print(len(list_1.intersection(list_2)))
